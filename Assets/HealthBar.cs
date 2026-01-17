996bd0569961ec6c45d627db8efb2b3d36d612aa
Spoofy <enchantedspoofy@gmail.com>
2026-01-17 14:10:03 -0500

    Health Bar (Not_Done)
    Health Bar Progress 1/2

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthBar : MonoBehaviour
+
+{
+    public Slider healthSlider;
+    public float maxHealth = 100f;
+    public float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (healthSlider.value != health)
+        {
+            healthSlider.value = health;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            takeDamage(10);
+        }
+
+    }
+
+    void takeDamage(float damage)
+    {
+        health -= damage;
+    }
+}
